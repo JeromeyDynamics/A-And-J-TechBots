e66f9a95505a9c2c5aceb6b7a9ff91a03e4d9175
Jerome Hillock <jeromeydawinner@gmail.com>
2025-07-26 06:38:38 -0400

    learned how to import and use the button from shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h2>
-        A&J TechBots
-      </h2>
+    <div className = "p-10">
+      <h2>A&J TechBots</h2>
+      <Button variant = "outline">BUTTON!</Button>
     </div>
   );
 }
