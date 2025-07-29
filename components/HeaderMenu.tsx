c0454ed5a889d59cd6856bc636915d51ea8b2eaa
Jerome Hillock <jeromeydawinner@gmail.com>
2025-07-29 15:47:32 -0400

    added links to the header menu with hover effects

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -1,8 +1,23 @@
 //using "rafce", a basic component with a div can be generated
-import React from 'react'
+import { headerData } from '@/constants';
+import Link from 'next/link';
+import React from 'react';
+import "../app/globals.css";
 
 const HeaderMenu = () => {
-  return <div>HeaderMenu</div>;
+  return (
+    <div className="flex w-1/3 items-center gap-5 text-sm capitalize font-semibold">
+      {headerData.map((item) => (
+        <Link
+          key={item.title}
+          href={item.href}
+          className="hover:text-dark hover-effect relative group"
+        >
+          {item.title}
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export default HeaderMenu;
---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+export const headerData = [
+    {title:"Home", href: "/"},
+    {title:"Robots", href: "/products"},
+    {title:"Courses", href: "/courses"},
+    {title:"About", href: "/about"},
+    {title:"Contact", href: "/contact"}
+];
